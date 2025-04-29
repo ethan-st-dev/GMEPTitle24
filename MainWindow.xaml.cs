09352156fb2b0920df1a0366c8aa391d2bf52e6a
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-04-29 10:11:09 -0700

    switching to observable collection from excel sheet

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -155,151 +155,143 @@ public partial class MainWindow : Window
             IWebElement lightingContainer = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div[name='organism_Table2_Row']")));
             var Lightings =  lightingContainer.FindElements(By.CssSelector("div[class='mod_multiField']"));
 
-            //Reading xlsx file
-            string filePath = Path.Combine(Environment.CurrentDirectory, "lti.xlsx");
-            ExcelPackage.License.SetNonCommercialPersonal("<GMEP>");
+            int rowCount = LightingList.Count;
 
-            using (var package = new ExcelPackage(new FileInfo(filePath)))
-            {
-                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-                int rowCount = worksheet.Dimension.Rows;
+            int lightingCount = Lightings.Count;
 
-                int lightingCount = Lightings.Count;
-
-                //Adjusting Box Count
-                if (lightingCount < rowCount - 1)
-                {    //Adding Boxes
-                    while (lightingCount < rowCount - 1)
-                    {
-                        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", AddLuminaireButton);
-                        lightingCount++;
-                    }
-                }
-                else if (lightingCount > rowCount - 1)
+            //Adjusting Box Count
+            if (lightingCount < rowCount)
+            {    //Adding Boxes
+                while (lightingCount < rowCount)
                 {
-                    //Removing Boxes
-                    foreach (var lighting in Lightings)
-                    {
-                        if (lightingCount > rowCount - 1)
-                        {
-                            var delete = lighting.FindElement(By.CssSelector("div[class='mod_supportControl']"));
-                            var deleteIcon = delete.FindElement(By.CssSelector("i"));
-                            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", deleteIcon);
-                            lightingCount--;
-                        }
-                    }
+                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", AddLuminaireButton);
+                    lightingCount++;
                 }
-                Lightings = lightingContainer.FindElements(By.CssSelector("div[class='mod_multiField']"));
-                //Editing Boxes
-                int row = 2;
+            }
+            else if (lightingCount > rowCount)
+            {
+                //Removing Boxes
                 foreach (var lighting in Lightings)
                 {
-                    //iterating through text entries
-                    var elements = lighting.FindElements(By.CssSelector("input[type='text']"));
-                    foreach (var element in elements)
+                    if (lightingCount > rowCount)
                     {
-                        string attributeValue = element.GetAttribute("placeholder");
-                        if (attributeValue != null && attributeValue.Contains("tag name", StringComparison.OrdinalIgnoreCase))
-                        {
-                            ((IJavaScriptExecutor)driver).ExecuteScript(@"
-                                arguments[0].value = arguments[1];
-                                arguments[0].dispatchEvent(new Event('input'));
-                                arguments[0].dispatchEvent(new Event('change'));
-                            ", element, worksheet.Cells[row, 1].Text);
-                        }
-                        if (attributeValue != null && attributeValue.Contains("luminaire description", StringComparison.OrdinalIgnoreCase))
-                        {
-                            ((IJavaScriptExecutor)driver).ExecuteScript(@"
-                                arguments[0].value = arguments[1];
-                                arguments[0].dispatchEvent(new Event('input'));
-                                arguments[0].dispatchEvent(new Event('change'));
-                            ", element, worksheet.Cells[row, 2].Text);
-                        }
-                        if (attributeValue != null && attributeValue.Contains("Watts per Luminaire", StringComparison.OrdinalIgnoreCase))
-                        {
-                            ((IJavaScriptExecutor)driver).ExecuteScript(@"
-                                arguments[0].value = arguments[1];
-                                arguments[0].dispatchEvent(new Event('input'));
-                                arguments[0].dispatchEvent(new Event('change'));
-                            ", element, worksheet.Cells[row, 5].Text);
+                        var delete = lighting.FindElement(By.CssSelector("div[class='mod_supportControl']"));
+                        var deleteIcon = delete.FindElement(By.CssSelector("i"));
+                        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", deleteIcon);
+                        lightingCount--;
+                    }
+                }
+            }
+            Lightings = lightingContainer.FindElements(By.CssSelector("div[class='mod_multiField']"));
+            //Editing Boxes
+            int row = 0;
+            foreach (var lighting in Lightings)
+            {
+                //iterating through text entries
+                var elements = lighting.FindElements(By.CssSelector("input[type='text']"));
+                foreach (var element in elements)
+                {
+                    string attributeValue = element.GetAttribute("placeholder");
+                    if (attributeValue != null && attributeValue.Contains("tag name", StringComparison.OrdinalIgnoreCase))
+                    {
+                        ((IJavaScriptExecutor)driver).ExecuteScript(@"
+                            arguments[0].value = arguments[1];
+                            arguments[0].dispatchEvent(new Event('input'));
+                            arguments[0].dispatchEvent(new Event('change'));
+                        ", element, LightingList[row].Tag);
+                    }
+                    if (attributeValue != null && attributeValue.Contains("luminaire description", StringComparison.OrdinalIgnoreCase))
+                    {
+                        ((IJavaScriptExecutor)driver).ExecuteScript(@"
+                            arguments[0].value = arguments[1];
+                            arguments[0].dispatchEvent(new Event('input'));
+                            arguments[0].dispatchEvent(new Event('change'));
+                        ", element, LightingList[row].Description);
+                    }
+                    if (attributeValue != null && attributeValue.Contains("Watts per Luminaire", StringComparison.OrdinalIgnoreCase))
+                    {
+                        ((IJavaScriptExecutor)driver).ExecuteScript(@"
+                            arguments[0].value = arguments[1];
+                            arguments[0].dispatchEvent(new Event('input'));
+                            arguments[0].dispatchEvent(new Event('change'));
+                        ", element, LightingList[row].Wattage);
+                    }
+                }
+                //Iterating Through dropdown entries
+                var dropdownElements = lighting.FindElements(By.CssSelector("div[class='selectWrapper']"));
+                foreach(var element in dropdownElements)
+                {
+                    var textbox = element.FindElement(By.CssSelector("input"));
+                    string placeholderValue = textbox.GetAttribute("placeholder");
+                    if (placeholderValue != null && placeholderValue.Contains("luminaire type.", StringComparison.OrdinalIgnoreCase))
+                    {
+                        switch (LightingList[row].TypeId) {
+                            case 1:
+                                var individualLuminaire = element.FindElement(By.CssSelector("li[data-value='TypicalLuminaire'"));
+                                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", individualLuminaire);
+                                break;
+                            case 2:
+                                var trackLighting = element.FindElement(By.CssSelector("li[data-value='TrackLighting'"));
+                                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", trackLighting);
+                                break;
+                            case 3:
+                                var smallAperatureColor = element.FindElement(By.CssSelector("li[data-value='SmallAperatureColor'"));
+                                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", smallAperatureColor);
+                                break;
                         }
                     }
-                    //Iterating Through dropdown entries
-                    var dropdownElements = lighting.FindElements(By.CssSelector("div[class='selectWrapper']"));
-                    foreach(var element in dropdownElements)
+                    if (placeholderValue != null && placeholderValue.Contains("general lighting", StringComparison.OrdinalIgnoreCase))
                     {
-                        var textbox = element.FindElement(By.CssSelector("input"));
-                        string placeholderValue = textbox.GetAttribute("placeholder");
-                        if (placeholderValue != null && placeholderValue.Contains("luminaire type.", StringComparison.OrdinalIgnoreCase))
+                        switch (LightingList[row].IsDecorative)
                         {
-                            switch (worksheet.Cells[row, 3].Text.ToLower()) {
-                                case "individual luminaire":
-                                    var individualLuminaire = element.FindElement(By.CssSelector("li[data-value='TypicalLuminaire'"));
-                                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", individualLuminaire);
-                                    break;
-                                case "track lighting":
-                                    var trackLighting = element.FindElement(By.CssSelector("li[data-value='TrackLighting'"));
-                                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", trackLighting);
-                                    break;
-                                case "small aperature and color changing":
-                                    var smallAperatureColor = element.FindElement(By.CssSelector("li[data-value='SmallAperatureColor'"));
-                                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", smallAperatureColor);
-                                    break;
-                            }
-                        }
-                        if (placeholderValue != null && placeholderValue.Contains("general lighting", StringComparison.OrdinalIgnoreCase))
-                        {
-                            switch (worksheet.Cells[row, 4].Text.ToLower())
-                            {
-                                case "x":
-                                    var AccentDecorativeLighting = element.FindElement(By.CssSelector("li[data-value='AccentDecorativeLighting'"));
-                                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", AccentDecorativeLighting);
-                                    break;
-                                default:  
-                                    var GeneralLighting = element.FindElement(By.CssSelector("li[data-value='GeneralLighting'"));
-                                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", GeneralLighting);
-                                    break;
-                            }
-                        }
-                        if (placeholderValue != null && placeholderValue.Contains("Wattage Determined", StringComparison.OrdinalIgnoreCase))
-                        {
-                            switch (worksheet.Cells[row, 6].Text.ToLower())
-                            {
-                                case "manufacturer spec sheet":
-                                    var ManufacturerSpec = element.FindElement(By.CssSelector("li[data-value='ManufacturerSpec'"));
-                                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", ManufacturerSpec);
-                                    break;
-                                case "default":
-                                    var Default = element.FindElement(By.CssSelector("li[data-value='CEC_Default'"));
-                                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", Default);
-                                    break;
-                                case "other":
-                                    var Other = element.FindElement(By.CssSelector("li[data-value='Other'"));
-                                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", Other);
-                                    break;
-                            }
-                        }
-                        /*if (placeholderValue != null && placeholderValue.Contains("Conditioned", StringComparison.OrdinalIgnoreCase))
-                        {
-                            //fill out
-                        }*/
-                        if (placeholderValue != null && placeholderValue.Contains("excluded from total", StringComparison.OrdinalIgnoreCase))
-                        {
-                            switch (worksheet.Cells[row, 8].Text.ToLower())
-                            {
-                                case "x":
-                                    var Yes = element.FindElement(By.CssSelector("li[data-value='Yes'"));
-                                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", Yes);
-                                    break;
-                                default:
-                                    var No = element.FindElement(By.CssSelector("li[data-value='No'"));
-                                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", No);
-                                    break;
-                            }
+                            case true:
+                                var AccentDecorativeLighting = element.FindElement(By.CssSelector("li[data-value='AccentDecorativeLighting'"));
+                                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", AccentDecorativeLighting);
+                                break;
+                            default:  
+                                var GeneralLighting = element.FindElement(By.CssSelector("li[data-value='GeneralLighting'"));
+                                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", GeneralLighting);
+                                break;
                         }
                     }
-                    row++;
-                }  
+                    if (placeholderValue != null && placeholderValue.Contains("Wattage Determined", StringComparison.OrdinalIgnoreCase))
+                    {
+                        switch (LightingList[row].WattageSourceId)
+                        {
+                            case 1:
+                                var ManufacturerSpec = element.FindElement(By.CssSelector("li[data-value='ManufacturerSpec'"));
+                                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", ManufacturerSpec);
+                                break;
+                            case 2:
+                                var Default = element.FindElement(By.CssSelector("li[data-value='CEC_Default'"));
+                                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", Default);
+                                break;
+                            case 3:
+                                var Other = element.FindElement(By.CssSelector("li[data-value='Other'"));
+                                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", Other);
+                                break;
+                        }
+                    }
+                    /*if (placeholderValue != null && placeholderValue.Contains("Conditioned", StringComparison.OrdinalIgnoreCase))
+                    {
+                        //fill out
+                    }*/
+                    if (placeholderValue != null && placeholderValue.Contains("excluded from total", StringComparison.OrdinalIgnoreCase))
+                    {
+                        switch (LightingList[row].IsExcluded)
+                        {
+                            case true:
+                                var Yes = element.FindElement(By.CssSelector("li[data-value='Yes'"));
+                                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", Yes);
+                                break;
+                            default:
+                                var No = element.FindElement(By.CssSelector("li[data-value='No'"));
+                                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", No);
+                                break;
+                        }
+                    }
+                }
+                row++;
             }
 
         });
