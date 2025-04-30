a6d555bcf2b863f37b90f6063a91445d5c4effed
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-04-30 14:27:37 -0700

    install script changed

--- Install.ps1
+++ Install.ps1
@@ -1,5 +1,5 @@
 ﻿$shortcut = (New-Object -ComObject Wscript.Shell).CreateShortcut("$([Environment]::GetFolderPath('Desktop'))\GMEPTitle24.lnk")
 $currentDir = Get-Location
-$shortcut.TargetPath = "$currentDir\bin\Debug\net9.0-windows\GMEPTitle24.exe"
+$shortcut.TargetPath = "$currentDir\bin\Debug\net8.0-windows7.0\GMEPTitle24.exe"
 $shortcut.IconLocation = "$currentDir\gmep.ico"
 $shortcut.Save()
